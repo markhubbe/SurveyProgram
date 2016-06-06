2b327c15816f9b776b4b849448df7ff2723fe7fa
Cara Hubbell <hubbell.cara@gmail.com>
2016-06-06 08:40:07 -0600

    Fixed special cases and plotting issues
    Fixed cases where there is no overlap and where there is 100% overlap.
    Fixed plot for case where there is no overlap (plotting is skipped)

--- rotated ellipse area calculator.R
+++ rotated ellipse area calculator.R
@@ -13,9 +13,9 @@
 #NOTE: User stores values for a1,b1,h1,k1,c1 and a2,b2,h2,k2,c2
 
 #For testing purposes, we create a specific example:
-h1<-1
+h1<-6
 k1<-2
-a1<-5
+a1<-1
 b1<-4
 c1<-10 #measured in degrees
 
@@ -85,44 +85,68 @@ e1keep<-e1[(cos(c2*pi/180)*(e1[,1]-h2)+sin(c2*pi/180)*(e1[,2]-k2))^2/a2^2 + (cos
 e2keep<-e2[(cos(c1*pi/180)*(e2[,1]-h1)+sin(c1*pi/180)*(e2[,2]-k1))^2/a1^2 + (cos(c1*pi/180)*(e2[,2]-k1)-sin(c1*pi/180)*(e2[,1]-h1))^2/b1^2 <= 1,]
 
 #for fun...
-plot(e1keep$x,e1keep$y, 
-     xlim=c(min(e1keep$x,e2keep$x),max(e1keep$x,e2keep$x)), 
-     ylim=c(min(e1keep$y,e2keep$y),max(e1keep$y,e2keep$y)),
-     xlab="",ylab=""
-)
-points(e2keep$x,e2keep$y)
+if(nrow(e1keep)!=0 & nrow(e2keep)!=0){
+  plot(e1keep$x,e1keep$y, 
+       xlim=c(min(e1keep$x,e2keep$x),max(e1keep$x,e2keep$x)), 
+       ylim=c(min(e1keep$y,e2keep$y),max(e1keep$y,e2keep$y)),
+       xlab="",ylab=""
+       )
+  points(e2keep$x,e2keep$y)
+}
 
 #--------------------------------  
 #Merge the two sets of coordinates into 'polycoord' - Mark's code :D
 
-polycoord<-matrix(0,(nrow(e1keep)+nrow(e2keep)),2)
-minE1X<-min(e1keep[,1])
-maxE1X<-max(e1keep[,1])
-walkDown=FALSE
-triggered=FALSE
-countE1=1
-countE2=1
-for(a in 1:nrow(polycoord)){
-  if(e1keep[countE1,1]==maxE1X && triggered==FALSE){
-    walkDown=TRUE
-    triggered=TRUE
-  }
-  
-  if (walkDown==FALSE){
-    polycoord[a,]<-as.matrix(e1keep[countE1,])
-    countE1<-countE1+1
-  }
-  if (walkDown==TRUE){
-    if(countE2<= nrow(e2keep)){
-      polycoord[a,]<-as.matrix(e2keep[countE2,])
-      countE2<-countE2+1
-    }else{
-      polycoord[a,]<-as.matrix(e1keep[countE1,])
-      countE1<-countE1+1
+if(nrow(e1keep)==0 & nrow(e2keep)==0){
+  #when there are no points contained within either ellipse, return 0
+  area<-0
+} else {
+  if(nrow(e1keep)==0){
+    #if ellipse2 is entirely within ellipse1, the overlap area
+    #is equal to the area of the inner ellipse
+    #The area is still calculated using a polygon
+    polycoord<-e2keep
+    } else {
+      if(nrow(e2keep)==0){
+        #if ellipse1 is entirely within ellipse2, the overlap area
+        #is equal to the area of the inner ellipse
+        #The area is still calculated using a polygon
+        polycoord<-e1keep
+        } else {
+          polycoord<-matrix(0,(nrow(e1keep)+nrow(e2keep)),2)
+          minE1X<-min(e1keep[,1])
+          maxE1X<-max(e1keep[,1])
+          walkDown=FALSE
+          triggered=FALSE
+          countE1=1
+          countE2=1
+          for(a in 1:nrow(polycoord)){
+            if(e1keep[countE1,1]==maxE1X && triggered==FALSE){
+              walkDown=TRUE
+              triggered=TRUE
+            }
+            
+            if (walkDown==FALSE){
+              polycoord[a,]<-as.matrix(e1keep[countE1,])
+              countE1<-countE1+1
+            }
+            if (walkDown==TRUE){
+              if(countE2<= nrow(e2keep)){
+                polycoord[a,]<-as.matrix(e2keep[countE2,])
+                countE2<-countE2+1
+              }else{
+                polycoord[a,]<-as.matrix(e1keep[countE1,])
+                countE1<-countE1+1
+              }
+            }
+          } 
+        }
     }
-    
-  }
 }
+
+
+    
+
 #--------------------------------  
 #Calculate the area of each ellipse
 area_e1<-pi*a1*b1
