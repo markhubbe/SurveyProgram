42cdb46dc0282de6d7d8f9c369bf108c97bc1d9c
markhubbe <mark.hubbe@gmail.com>
2016-06-06 09:55:37 -0400

    Test
    just trying to test!

---
+++ surveyEfficacy.R
@@ -0,0 +1,19 @@
+#This function will do mutiple things:
+#1. Plot random number of sites in an area.
+#2. Plot random number of articfacts within each ellipsis.
+#3. Superimpose grids of survey pits to test how many pits hit sites and articacts.
+
+surveyEfficacy<-function(area,SiteArea,SiteDensity){
+  #Variable propoerties:
+  ##Area: vectors with horizontal and vertical size of area surveyed.
+  ##SiteArea: it will one of two: either vector with 4 values: min, max, mean (or median), and standard deviation. 
+    #Or one single value for uniform sites.  
+    #Sites will all be ellipses with radii not too different and random angles.
+    #sites will not overlap by much, for now.
+  ##SiteDensity: can be either one value or a vector with 2 values (min and max) to create a range of densities. 
+  
+  
+  
+  
+  
+}
