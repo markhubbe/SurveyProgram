fe750a1ccaac89c27819fe400b28bf6ced4e40a7
Cara <hubbell.cara@gmail.com>
2017-05-18 09:07:59 -0600

    Updated Point Cloud Generator
    Updated to be a function and to include type selector for uniformly and
    normally distributed distances

--- Point Cloud Generator.R
+++ Point Cloud Generator.R
@@ -1,39 +1,38 @@
-
-#This script generates a cloud of points within a rotated and shifted ellipse.
-
-#-----------------------------------
-#Ellipse Characteristics
-
-#Specific values selected for script testing:
-
-a<-3 #x-direction axial length
-b<-5 #y-direction axial length
-alpha<-pi/4 #ellipse angle of rotation IN RADIANS - does this need to be degrees?? If so, no problem :)
-h<-1 #x-direction shift
-k<--3 #y-direction shift
+#This script generates cloud of points within a given ellipse.
+pointCloud<-function(a, b, h, k, theta, n, type){
   
-#-----------------------------------
-#Density and Volume of Point Cloud
-n<-10000 #number of points in cloud
-angl<-runif(n, 0, pi) #list of n random angles from 0 to pi
-magn<-runif(n, -1, 1) #starting with a uniform dist - MOD THIS FOR NORMAL DIST...
+  # a = x-direction axial length / x-direction radius
+  # b = y-direction axial length / y-direction radius
+  # theta = ellipse angle of rotation IN RADIANS
+  # h = x-direction shift / x-coordinate of center
+  # k = y-direction shift / y-coordinate of center
+  # n = number of points in cloud
+  # type = "unif" or "norm"
+  
+  #Generate list of n random angles from 0 to pi
+  angl<-runif(n, 0, pi) 
+  
+  #Determine the distance to the ellipse from the center at each angle theta
+  d<-sqrt(a^2*b^2/((sin(angl))^2*(a^2-b^2)+b^2))
 
-#Determine the distance to the ellipse from the center at each angle theta
-d<-sqrt(a^2*b^2/((sin(angl))^2*(a^2-b^2)+b^2))
+  #Generate list of n random distances based on distr type
+  if(type == "norm"){
+    magn<-rnorm(n, mean=0, sd=0.5102) #95% of points are contained within the ellipse
+    } else if (type == "unif"){
+      magn<-runif(n, -1, 1) #all points are contained within the ellipse
+    } else {
+      print("Type is invalid. Please choose unif or norm.")
+    }
 
-#Generate Points Without Rotation
-xval_unrot<-magn*d*cos(angl)
-yval_unrot<-magn*d*sin(angl)
+  #Generate cloud points without rotation or translation
+  xval_unrot<-magn*d*cos(angl)
+  yval_unrot<-magn*d*sin(angl)
 
-#Rotate points through angle alpha, then shift by (h,k)
-xval<-xval_unrot*cos(alpha)-yval_unrot*sin(alpha)+h
-yval<-xval_unrot*sin(alpha)+yval_unrot*cos(alpha)+k
+  #Rotate cloud points through angle theta, then translate by (h,k) to match ellipse
+  xval<-xval_unrot*cos(theta)-yval_unrot*sin(theta)+h
+  yval<-xval_unrot*sin(theta)+yval_unrot*cos(theta)+k
 
-#-----------------------------------
-#Plot Points
-plot(0,
-     xlim=c(min(xval),max(xval)), 
-     ylim=c(min(yval),max(yval)),
-     xlab="",ylab=""
-)
-points(xval,yval)
+  #Plot Points
+  plot(xval, yval, xlab="", ylab="")
+  
+}
